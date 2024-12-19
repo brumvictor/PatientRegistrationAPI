791e20fc8851b9f71a8758282fc495ba84fc8be8
Victor Brum <83709796+brumvictor@users.noreply.github.com>
2024-12-19 17:05:05 -0300

    Add Project

---
+++ src/main/java/com/patientregistrationapi/PatientRegistrationApiApplication.java
@@ -0,0 +1,13 @@
+package com.patientregistrationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientRegistrationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientRegistrationApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/patientregistrationapi/PatientRegistrationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patientregistrationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientRegistrationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
