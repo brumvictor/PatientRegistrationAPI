afa7c24d6facc92e6fc51204431bb12cc981a5d6
Victor Brum <83709796+brumvictor@users.noreply.github.com>
2024-12-31 12:44:58 -0300

    Delete

--- src/main/java/com/patientregistrationapi/controllers/PatientController.java
+++ src/main/java/com/patientregistrationapi/controllers/PatientController.java
@@ -3,7 +3,9 @@ package com.patientregistrationapi.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -42,5 +44,11 @@ public class PatientController {
 	public void update(@RequestBody @Valid UpdatePatientDto dto) {
 		var patient = repository.getReferenceById(dto.id());
 		patient.updateInfo(dto);
+	}
+	
+	@DeleteMapping("/{id}")
+	@Transactional
+	public void delete (@PathVariable Long id) {
+		repository.deleteById(id);
 	}
 }
