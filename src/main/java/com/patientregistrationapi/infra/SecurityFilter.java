d698cc4d9c2ed5a57875265052a81666ae3b9959
Victor Brum <83709796+brumvictor@users.noreply.github.com>
2025-01-15 09:35:25 -0300

    Add SecurityFilter and JWTVerifier method

---
+++ src/main/java/com/patientregistrationapi/infra/SecurityFilter.java
@@ -0,0 +1,42 @@
+package com.patientregistrationapi.infra;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+	
+	@Autowired
+	private TokenService tokenService;
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		
+		var tokenJWT = getToken(request);
+		var subject = tokenService.getSubject(tokenJWT);
+		
+		filterChain.doFilter(request, response);
+		
+	}
+
+	private String getToken(HttpServletRequest request) {
+		var authorizationHeader = request.getHeader("Authorization");
+		
+		if (authorizationHeader == null)
+		{
+			throw new RuntimeException("Token não enviado.");
+		}
+		
+		return authorizationHeader;
+	}
+	
+}
--- src/main/java/com/patientregistrationapi/infra/TokenService.java
+++ src/main/java/com/patientregistrationapi/infra/TokenService.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.patientregistrationapi.users.User;
 
 @Service
@@ -28,6 +29,20 @@ public class TokenService {
 		        .sign(algorithm);
 		} catch (JWTCreationException exception){
 		    throw new RuntimeException("Erro ao gerar o token", exception);
+		}
+	}
+	
+	public String getSubject(String TokenJWT) {
+		try {
+			var algorithm = Algorithm.HMAC256(secret);
+		    return JWT.require(algorithm)
+		    	.withIssuer("Patient_Registration_api")
+		        .build()
+		        .verify(TokenJWT)
+		        .getSubject();
+		        
+		} catch (JWTVerificationException exception){
+		    throw new RuntimeException("Token inválido ou expirado!");
 		}
 	}
 
